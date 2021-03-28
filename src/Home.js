68ecb0977a5c4e82e104354263e75a4072512056
babdurrahman78 <babdurrahman78@gmail.com>
2021-03-29 00:10:58 +0700

    UseEffecth to fetch data

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,33 @@
-import { useState } from 'react';
-import BlogList from './BlogList';
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 // imrs
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'Tajwid',body: 'puntenslur...', author: 'Bilal', id: 1},
-        {title: 'Aqidah',body: 'puntenslur...', author: 'Nur Fajri', id: 2},
-        {title: 'Fiqih',body: 'puntenslur...', author: 'Hadrami', id: 3},
-        {title: 'Tafsir',body: 'puntenslur...', author: 'Hadrami', id: 4},
-        {title: 'Sharaf',body: 'puntenslur...', author: 'Hadrami', id: 5},
-    ])
-    
-    const deleteBlog = (id) => {
-        const deletedBlogs = blogs.filter((blog) => blog.id !== id);
-        setBlogs(deletedBlogs);
-    }
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
-    return ( 
-        <div className="home">
-            <BlogList blogs = {blogs} title = 'All Blog' deleteBlog={deleteBlog}/>
-        </div>
-     );
-}
- 
+  const deleteBlog = (id) => {
+    const deletedBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(deletedBlogs);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+        setIsPending(false);
+      });
+  }, []);
+
+  return (
+    <div className="home">
+      {/* nullish coalescing */}
+      {isPending && <p>Loading... please wait</p>}
+      {blogs && <BlogList blogs = {blogs} title = 'All Blog' deleteBlog={deleteBlog}/>}
+    </div>
+  );
+};
+
 export default Home;
