750d6d2859c9a2acdba299456753522670ebf7ee
babdurrahman78 <babdurrahman78@gmail.com>
2021-04-14 12:58:54 +0700

    adding react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import Home from './Home';
-import Navbar from './Navbar';
+import Home from "./Home";
+import Create from "./Create";
+import Navbar from "./Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   // you can write any javascript syntax here
 
   return (
-    <div className="App">
-      <Navbar /> 
-      <div className="content">
-          <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
