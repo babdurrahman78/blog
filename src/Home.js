bc69ff707fb94d804f22c837f28575fbd0d9c3dc
babdurrahman78 <babdurrahman78@gmail.com>
2021-04-14 12:45:24 +0700

    create useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,15 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch"
 // imrs
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
+  const {data:blogs, setData, isPending, error} = useFetch('http://localhost:8000/blogs')
 
   const deleteBlog = (id) => {
     const deletedBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(deletedBlogs);
+    setData(deletedBlogs);
   };
 
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        if(!res.ok) {
-            throw new Error('Data tidak ditemukan')
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsPending(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setIsPending(false);
-      })
-  }, []);
+  
 
   return (
     <div className="home">
---
+++ src/useFetch.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Data tidak ditemukan");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsPending(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsPending(false);
+      });
+  }, [url]);
+
+  return { data, setData, isPending, error}
+};
+
+export default useFetch;
