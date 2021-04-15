c9ad85acd8f9cf6869b2e663080855db78996f39
babdurrahman78 <babdurrahman78@gmail.com>
2021-04-15 08:30:28 +0700

    linter applied

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,23 +1,22 @@
 import { Link } from "react-router-dom";
 
 const BlogList = ({ blogs, title, deleteBlog }) => {
-    return (
-        <div className="blog-list">
-            <h2>{title}</h2>
-            {blogs.map((blog) => {
-                return (
-                    <div className="blog-previews" key={blog.id}>
-                        <Link to={`/blogs/${blog.id}`}>
-
-                            <h2>{blog.title}</h2>
-                            <p>Written by {blog.author}</p>
-                            <button onClick={() => deleteBlog(blog.id)}> Delete </button>
-                        </Link>
-                    </div>
-                );
-            })}
-        </div>
-    );
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => {
+        return (
+          <div className="blog-previews" key={blog.id}>
+            <Link to={`/blogs/${blog.id}`}>
+              <h2>{blog.title}</h2>
+              <p>Written by {blog.author}</p>
+              <button onClick={() => deleteBlog(blog.id)}> Delete </button>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default BlogList;
