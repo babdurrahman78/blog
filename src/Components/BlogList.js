f7fd0040bb098d571ca88aa40f69da56c9124d0a
babdurrahman78 <babdurrahman78@gmail.com>
2021-04-17 11:22:28 +0700

    change display bloglist to flex

--- src/Components/BlogList.js
+++ src/Components/BlogList.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Card, Button, CardTitle, CardSubtitle } from 'reactstrap';
+import { Card, Button, CardTitle, CardSubtitle, Row, Col } from "reactstrap";
 import "../css/BlogList.css";
 
 const BlogList = ({ blogs, title }) => {
@@ -7,19 +7,23 @@ const BlogList = ({ blogs, title }) => {
     <div>
       <div className="blog-list">
         <h2 className="blog-list-title">{title}</h2>
-        {blogs.map((blog) => {
-          return (
-            <div className="blog-previews" key={blog.id}>
-              <Card body>
-                <CardTitle tag="h5">{blog.title}</CardTitle>
-                <CardSubtitle tag="h6" className="mb-2 text-muted">
-                  Written by {blog.author}
-                </CardSubtitle>
-                <Link to={`/blogs/${blog.id}`}><Button color="success">Read More..</Button></Link>
-              </Card>
-            </div>
-          );
-        })}
+        <Row>
+          {blogs.map((blog) => {
+            return (
+              <Col sm="6">
+                <Card body className="blog-previews" key={blog.id}>
+                  <CardTitle tag="h5">{blog.title}</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">
+                    Written by {blog.author}
+                  </CardSubtitle>
+                  <Link to={`/blogs/${blog.id}`}>
+                    <Button color="success">Read More..</Button>
+                  </Link>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
       </div>
     </div>
   );
