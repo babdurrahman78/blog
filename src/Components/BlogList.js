4cbdb8d046361441fecf169675adb533a12cd51a
babdurrahman78 <babdurrahman78@gmail.com>
2021-04-18 05:25:04 +0700

    add CardImg

--- src/Components/BlogList.js
+++ src/Components/BlogList.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Card, Button, CardTitle, CardSubtitle, Row, Col } from "reactstrap";
+import { Card, Button, CardTitle, CardSubtitle, CardImg, Row, Col } from "reactstrap";
 import "../css/BlogList.css";
 
 const BlogList = ({ blogs, title }) => {
@@ -13,6 +13,7 @@ const BlogList = ({ blogs, title }) => {
               <Col sm="6">
                 <Card body className="blog-previews" key={blog.id}>
                   <CardTitle tag="h5">{blog.title}</CardTitle>
+                  <CardImg className="my-2" top width="100%" src={blog.img} alt="Card image cap" />
                   <CardSubtitle tag="h6" className="mb-2 text-muted">
                     Written by {blog.author}
                   </CardSubtitle>
